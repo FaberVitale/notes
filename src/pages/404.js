009782b2d943f45130ae117309b3c1bf2cf6abd2
FaberVitale <faber.vitale@gmail.com>
2018-06-01 12:31:22 +0200

    add emoji to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404 NOT FOUND</h1>
+    <p>
+      You just hit a route that doesn&#39;t exist...{" "}
+      <span role="img" aria-label="sadness">
+        😭
+      </span>
+    </p>
   </div>
 );
 
