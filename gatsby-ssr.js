85245194878c2788f08ae695756222a838e54f90
FaberVitale <faber.vitale@gmail.com>
2018-06-01 11:19:24 +0200

    minify jss-ssr css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,18 @@
 /* from https://github.com/mui-org/material-ui/blob/master/examples/gatsby/gatsby-ssr.js */
 /* eslint-disable react/no-danger */
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { JssProvider } from 'react-jss'
-import getPageContext from './src/getPageContext'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { JssProvider } from "react-jss";
+import getPageContext from "./src/getPageContext";
+import CleanCss from "clean-css";
 
 exports.replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
-  setHeadComponents,
+  setHeadComponents
 }) => {
   // Get the context of the page to collected side effects.
-  const pageContext = getPageContext()
+  const pageContext = getPageContext();
 
   const bodyHTML = renderToString(
     <JssProvider
@@ -19,20 +20,25 @@ exports.replaceRenderer = ({
       generateClassName={pageContext.generateClassName}
     >
       {React.cloneElement(bodyComponent, {
-        pageContext,
+        pageContext
       })}
     </JssProvider>
-  )
+  );
 
-  replaceBodyHTMLString(bodyHTML)
+  const minifier = new CleanCss({});
+
+  const minified = minifier.minify(pageContext.sheetsRegistry.toString())
+    .styles;
+
+  replaceBodyHTMLString(bodyHTML);
   setHeadComponents([
     <style
       type="text/css"
       id="server-side-jss"
       key="server-side-jss"
       dangerouslySetInnerHTML={{
-        __html: pageContext.sheetsRegistry.toString(),
+        __html: minified
       }}
-    />,
-  ])
-}
+    />
+  ]);
+};
